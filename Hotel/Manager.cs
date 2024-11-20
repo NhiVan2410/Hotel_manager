5235ec508a863c4967c8b6b93e07b0ae35971ee4
Nhi Thảo <thaonhivanpham2005@gmail.com>
2024-11-20 19:08:54 +0700

    Xong class sườn đủ trong file thầy đưa

---
+++ Hotel/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+   public  class Manager : Person
+    {
+        public Manager(int id, string name, string username, string password, string contactInfo)
+           : base(id, name, username, password, contactInfo)
+        {
+        }
+        // Method QLIKS
+        public void ManageHotel()
+        {
+            Console.WriteLine("Managing the hotel...");
+        }
+    }
+}
