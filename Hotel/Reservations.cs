ccfe123fe3e6be3c95fb6a2353a70a08f60e7e64
Nhi Thảo <thaonhivanpham2005@gmail.com>
2024-11-20 19:35:46 +0700

    Xong sql rr hunf ơi

---
+++ Hotel/Reservations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public  class Reservations
+    {
+        public int ReservationID { get; set; }
+        public int GuestID { get; set; }
+        public int RoomID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public string Feedback { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool IsDeleted { get; private set; }
+
+        public Reservations(int guestID, int roomID, DateTime checkInDate, DateTime checkOutDate)
+        {
+            GuestID = guestID;
+            RoomID = roomID;
+            BookingDate = DateTime.Now;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now;
+            IsDeleted = false;
+        }
+        public void MarkAsDeleted()
+        {
+            IsDeleted = true;
+
+        }
+        public void AddFeedback(string feedback)
+        {
+            Feedback = feedback;
+        }
+    }
+}
