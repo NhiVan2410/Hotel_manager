3c32a6ee685ace44dfe4409f3f582dc67bfbbea3
NhiVan2410 <23110049@student.hcmute.edu.vn>
2024-11-21 13:58:42 +0700

    BookingServices // quản lí đặt phòng cho reservation add từ linq

---
+++ Hotel/BookingService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class BookingService // quản lí bookingg
+    {
+        private readonly HotelDbContext _context;
+
+        public BookingService()
+        {
+            _context = new HotelDbContext();
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            return _context.Reservations.Where(r => r.IsDeleted == 0).ToList();
+        }
+
+        public void AddReservation(Reservation reservation)
+        {
+            _context.Reservations.InsertOnSubmit(reservation);
+            _context.SubmitChanges();
+        }
+
+        public void UpdateReservation(Reservation updatedReservation)
+        {
+            var reservation = _context.Reservations.FirstOrDefault(r => r.ReservationID == updatedReservation.ReservationID);
+            if (reservation != null)
+            {
+                reservation.CheckInDate = updatedReservation.CheckInDate;
+                reservation.CheckOutDate = updatedReservation.CheckOutDate;
+                _context.SubmitChanges();
+            }
+        }
+    }
+}
