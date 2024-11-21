954720d71556aa3d98eb37963668bf6ae5c73e87
NhiVan2410 <23110049@student.hcmute.edu.vn>
2024-11-21 14:15:30 +0700

    Guestservice và Resrvationservice từ LinQ vào

---
+++ Hotel/GuestService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    public class GuestService // quản lí Guest từ linQ add vào
+    {
+        private readonly HotelDbContext _context;
+
+        public GuestService()
+        {
+            _context = new HotelDbContext();
+        }
+
+        public List<Guest> GetAllGuests()
+        {
+            return _context.Guests.Where(g => g.IsDeleted == false).ToList();
+        }
+
+        public void AddGuest(Guest guest)
+        {
+            _context.Guests.InsertOnSubmit(guest);
+            _context.SubmitChanges();
+        }
+
+        public void DeleteGuest(int guestId)
+        {
+            var guest = _context.Guests.FirstOrDefault(g => g.GuestID == guestId);
+            if (guest != null)
+            {
+                guest.IsDeleted = true;
+                _context.SubmitChanges();
+            }
+        }
+    }
+}
---
+++ Hotel/ReservationServiceManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel
+{
+    /// uản lý riêng các dịch vụ bổ sung liên kết với đặt phòng
+    public class ReservationServiceManager
+    {
+        private readonly HotelDbContext _context;
+
+        public ReservationServiceManager()
+        {
+            _context = new HotelDbContext();
+        }
+
+        public List<ReservationService> GetAllReservationServices()
+        {
+            return _context.ReservationServices.Where(rs => rs.IsDeleted ==0).ToList();
+        }
+
+        public void AddServiceToReservation(int reservationId, int serviceId, int quantity)
+        {
+            var reservationService = new ReservationService
+            {
+                ReservationID = reservationId,
+                ServiceID = serviceId,
+                Quantity = quantity,
+                CreatedAt = DateTime.Now,
+                IsDeleted = 1 // false
+            };
+
+            _context.ReservationServices.InsertOnSubmit(reservationService);
+            _context.SubmitChanges();
+        }
+    }
+}
